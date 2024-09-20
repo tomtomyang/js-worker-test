1bae0e251a9fae4398f5a81e0009e06f319b1b36
tomtomyang <tomtomyang@tencent.com>
2024-09-20 16:34:43 +0800

    feat: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/*.test.js',
+  ],
   collectCoverage: true,
   collectCoverageFrom: [
    'test/**/*.{js,jsx,ts,tsx}',
-  ]
+  ],
 };
